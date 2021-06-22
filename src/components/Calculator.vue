8e4ee93d4df6fc5e524584df5d2ae98628c1498f
Giovanni Galiero <gio@Giovannis-MacBook-Pro.local>
2021-06-22 14:14:34 +0100

    feat(all): init

---
+++ src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Calculator',
+  components: {},
+})
+</script>
+
+<style></style>
